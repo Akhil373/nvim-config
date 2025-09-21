67e880cf5ee2ea843393a37fc21ed27e11b61bf9
Akhil <133588800+Akhil373@users.noreply.github.com>
2025-09-21 17:21:31 +0530

    idk

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,53 +1,56 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        local sources = {
-            require("none-ls.formatting.ruff").with({
-                extra_args = { "--extend-select", "I" },
-            }),
-            require("none-ls.formatting.ruff_format"),
-            null_ls.builtins.formatting.prettier.with({
-                filetypes = { "json", "yaml", "markdown" },
-            }),
-            null_ls.builtins.formatting.clang_format.with({
-                args = { "-i", "--style = {BasedOnStyle: Google, IndentWidth: 4}" },
-            }),
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.gofumpt,
-            null_ls.builtins.formatting.goimports,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local sources = {
+			require("none-ls.formatting.ruff").with({
+				extra_args = { "--extend-select", "I" },
+			}),
+			require("none-ls.formatting.ruff_format"),
+			null_ls.builtins.formatting.prettier.with({
+				filetypes = { "json", "yaml", "markdown" },
+			}),
+			null_ls.builtins.formatting.clang_format.with({
+				args = {
+					"-i",
+					"--style = {fallback-style=webkit, IndentWidth: 4, TabWidth: 4, AccessModifierOffset: 0, IndentAccessModifier: true}",
+				},
+			}),
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.gofumpt,
+			null_ls.builtins.formatting.goimports,
 
-            null_ls.builtins.diagnostics.pylint,
-            null_ls.builtins.diagnostics.golangci_lint,
-            -- null_ls.builtins.diagnostics.ruff,
-            -- null_ls.builtins.diagnostics.eslint_d,
-        }
+			null_ls.builtins.diagnostics.pylint,
+			null_ls.builtins.diagnostics.golangci_lint,
+			-- null_ls.builtins.diagnostics.ruff,
+			-- null_ls.builtins.diagnostics.eslint_d,
+		}
 
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-        null_ls.setup({
-            sources = sources,
+		null_ls.setup({
+			sources = sources,
 
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({ async = false, bufnr = bufnr })
-                        end,
-                    })
-                end
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false, bufnr = bufnr })
+						end,
+					})
+				end
 
-                -- keymap for formatting (buffer-local)
-                vim.keymap.set("n", "<A-S-f>", function()
-                    vim.lsp.buf.format({ async = true })
-                end, { buffer = bufnr })
-            end,
-        })
-    end,
+				-- keymap for formatting (buffer-local)
+				vim.keymap.set("n", "<A-S-f>", function()
+					vim.lsp.buf.format({ async = true })
+				end, { buffer = bufnr })
+			end,
+		})
+	end,
 }
