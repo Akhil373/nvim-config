aa2af10c4f3fe13b0c3f92211fdad2ba2aa37e78
Akhil373 <akhil2005.m@gmail.com>
2026-05-19 17:42:29 +0530

    config changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("kanagawa")
+
+-- Save session before quitting
+vim.keymap.set("n", "<leader>rs", ":mksession!<CR>", { noremap = true })
+-- reload nvim config: "https://www.reddit.com/r/neovim/comments/15gihnd/neat_trick_for_restarting_neovim_fast_when_using/"
+vim.api.nvim_set_keymap("n", "<leader>rr", ":cq<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-z>", "<nop>")
 
@@ -20,3 +25,6 @@ vim.keymap.set("n", "<leader>e", function()
 end, { desc = "Toggle Snacks Explorer" })
 
 vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
