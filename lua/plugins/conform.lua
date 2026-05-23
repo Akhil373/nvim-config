69901dd6b25bf1291ba628406c843742bedf844f
Akhil <133588800+Akhil373@users.noreply.github.com>
2026-05-23 15:47:07 +0530

    formatters complete setup

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,27 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				go = { "gofumpt" },
-			},
-		},
-	},
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                go = { "goimports", "gofumpt" },
+
+                json = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                html = { "prettier" },
+                css = { "prettier" },
+
+                lua = { "stylua" },
+
+                python = { "black" },
+
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+
+                sh = { "shfmt" },
+            },
+        },
+    },
 }
